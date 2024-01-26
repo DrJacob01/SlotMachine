3fba8bb2161961eac0dcffa5dc34a0dd2efd6aca
DrJacob01 <119816008+DrJacob01@users.noreply.github.com>
2024-01-26 13:03:18 +0000

    Add files via upload

---
+++ SlotMachine/SlotMachine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+String[] fruitList = {"Apple", "Blueberry", "Carrot"};
+
+Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~");
+Console.WriteLine("Are you ready to spin?");
+Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~");
+Thread.Sleep(1000);
+
+
+Random dice = new Random();     //create a var called dice, this is a random var thingy.
+
+for (int i = 0; i < 3; i++) // Loop for three spins
+{
+    string randPicker = fruitList[dice.Next(0, 3)]; // Pick a random fruit from the list
+    Console.WriteLine(randPicker);  // Display the randomly picked fruit
+    Thread.Sleep(1000);
+}
